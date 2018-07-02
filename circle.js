f3850208ba3b57c2f7ed14cc97d30a9b6c02489f
Sai Eswari <eswari795@gmail.com>
2018-07-02 12:40:28 +0530

    added circle and temp files

---
+++ circle.js
@@ -0,0 +1,12 @@
+function calcCircumfrence(radius){
+    const pi=3.14;
+   var c=2*pi*radius;
+   console.log("The Circumference is "+c);
+}
+calcCircumfrence(3);
+function calcArea(radius){
+ const pi=3.14;
+ var a=pi*radius*radius;
+ console.log("The Area is "+a);
+}
+calcArea(3);
---
+++ temp.js
@@ -0,0 +1,12 @@
+function celsiusToFahrenheit(){
+    var celsius=10;
+    var f=celsius*9/5+32;
+    console.log(celsius+"'C is"+f+"'F");
+}
+celsiusToFahrenheit();
+function fahrenheitToCelsius(){
+    var f=10;
+    var c=f-32*5/9;
+    console.log(f+"'F is"+c+"'C");
+}
+fahrenheitToCelsius();

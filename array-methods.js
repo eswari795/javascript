cd40a2cb27ca5ae8d53600d5c7b1321499142e30
Sai Eswari <eswari795@gmail.com>
2018-07-03 11:32:45 +0530

    added array-methods file

---
+++ array-methods.js
@@ -0,0 +1,19 @@
+//from()
+var x=Array.from("eshu",function(p){
+return p.toUpperCase();
+});
+console.log(x);
+
+//isArray()
+var x=[1,2,3,4];
+console.log(Array.isArray(x));
+
+//of()
+var q=Array.of(1,2,3);
+console.log(q);
+console.log(q.length);
+
+// var t= new Array(1,2,3);
+// console.log(t);
+// console.log(t.length);
+
